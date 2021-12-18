165432ec11505ad5c39326628a4ff1c5a117e070
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2021-12-18 20:01:07 +0100

    OMG Svelte works with a real setup now

--- src/PlayerList.svelte
+++ src/PlayerList.svelte
@@ -1,9 +1,14 @@
+<script>
+  export let players;
+  export let lobbyId;
+</script>
+
 <main class="px-3">
   <div id="pre-lobby">
-    <h1>Joined lobby {lobby}</h1>
+    <h1>Joined lobby {lobbyId}</h1>
     <h4>Players:</h4>
     <ul class="list-group">
-      {#each players as player, i}
+      {#each players as player}
       <li class="list-group-item">🎮 {player.name}</li>
       {:else}
       <li>No players?</li>
