1f7b2925460cd9077d4f814ad246d302a6fd4bc7
veit <veit.frick@aau.at>
2021-12-18 22:50:54 +0100

    Scorelist added

---
+++ src/ScoreList.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let scores;
+  export let you;
+</script>
+
+<main class="px-3">
+  <div id="pre-lobby">
+    <h1>Player Scores</h1>
+    <ul class="list-group">
+      {#each Object.entries(scores).sort((a,b) => b[1]-a[1]) as [key, value]}
+        {#if key === you}
+          <li class="list-group-item active">🎮 You : 🌟 {value}</li>
+        {:else}
+          <li class="list-group-item">🎮 {key} : 🌟 {value}</li>
+        {/if}
+        {:else}
+      <li>No players?</li>
+      {/each}
+    </ul>
+  </div>
+</main>
+
