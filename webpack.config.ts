696828eff1677e7685489ec995e51e44e7549b51
Christoph Wedenig <christoph.wedenig@dynatrace.com>
2021-12-19 16:03:43 +0100

    HOTFIX file loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -150,7 +150,16 @@ const config: Configuration = {
 				test: /\.ts$/,
 				use: 'ts-loader',
 				exclude: /node_modules/
-			}
+			},
+
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
 		]
 	},
 	devServer: {
